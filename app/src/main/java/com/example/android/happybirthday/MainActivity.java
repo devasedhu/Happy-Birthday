48226d90fd77404dcb35e76e5b37027b77c67235
Deva Sedhu <devasedhu@gmail.com>
2019-11-30 23:52:48 +0530

    added text view

--- app/src/main/java/com/example/android/happybirthday/MainActivity.java
+++ app/src/main/java/com/example/android/happybirthday/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView;
     }
 
     @Override
